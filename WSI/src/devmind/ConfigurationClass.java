fffe829454a19ee92c4ebb225f4a478e63d43989
paunioana <94004958+paunioana@users.noreply.github.com>
2023-04-05 19:15:37 +0300

    Delete WSI directory

--- WSI/src/devmind/ConfigurationClass.java
+++
@@ -1,9 +0,0 @@
-package devmind;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class ConfigurationClass {
-}
--- WSI/src/devmind/GlobalService.java
+++
@@ -1,4 +0,0 @@
-package devmind;
-
-public class GlobalService {
-}
